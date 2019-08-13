7917d0bab3ededf699f5cee933cfbfa50d2f7d16
297179121 <297179121@qq.com>
2019-08-13 21:49:54 +0800

    develope Singleton

---
+++ src/study/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package study.singleton;
+
+public class Singleton {
+
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if( uniqueInstance==null ){
+            synchronized (Singleton.class){
+                if( uniqueInstance==null ){
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+
+}
