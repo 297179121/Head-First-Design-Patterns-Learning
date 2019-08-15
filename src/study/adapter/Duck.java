a90bee2c1bcb8461ce8cd925d00531f89292ddcf
297179121 <297179121@qq.com>
2019-08-15 22:49:44 +0800

    外观模式+适配器模式

---
+++ src/study/adapter/Duck.java
@@ -0,0 +1,6 @@
+package study.adapter;
+
+public interface Duck {
+    void quack();
+    void fly();
+}
---
+++ src/study/adapter/Turkey.java
@@ -0,0 +1,6 @@
+package study.adapter;
+
+public interface Turkey {
+    void gobble();
+    void fly();
+}
